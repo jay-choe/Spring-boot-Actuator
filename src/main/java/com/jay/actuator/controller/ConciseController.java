5bcf5850552429acc6613124f209df5009752cc0
jay-choe <jay0764@naver.com>
2021-09-07 02:57:07 +0900

    Add controller, config for security, custom health indicator

---
+++ src/main/java/com/jay/actuator/controller/ConciseController.java
@@ -0,0 +1,28 @@
+package com.jay.actuator.controller;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConciseController {
+    @GetMapping("/")
+    public String sayHello(@RequestParam(value = "name", defaultValue = "Guest") String name) {
+        return "Hello " + name + "!!";
+    }
+
+    @GetMapping("/slowApi")
+    public String timeConsumingAPI(@RequestParam(value = "delay", defaultValue = "0") Integer delay) throws InterruptedException {
+        if(delay == 0) {
+            Random random = new Random();
+            delay = random.nextInt(10);
+        }
+
+        TimeUnit.SECONDS.sleep(delay);
+        return "Result";
+    }
+
+}
